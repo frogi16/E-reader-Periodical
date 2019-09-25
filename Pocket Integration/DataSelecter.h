95707dcf24cce5d53b4265e3114ce8290aaf45e7
Przemyslaw Skrobot <44038381+frogi16@users.noreply.github.com`>
2019-09-25 21:30:13 +0200

    Turn DataSelecter class into a template
    Single tree walker object can simultaneously traverse only one tree, so
    in order to make DataSelecter thread-safe each selection has to use
    unique tree walker.
    
    This change made DataSelecter basically a stateless object and allowed
    to turn the class into a function. Additional refactoring and use of
    parameter pack simplified implementation and made future extensions
    effortless.
    
     - DataSelecter - remove this class
     - EbookPeriodical::selectNodes - add new template function in
    EbookPeriodical namespace to replace DataSelecter

--- Pocket Integration/DataSelecter.h
+++ Pocket Integration/DataSelecter.h
@@ -1,20 +1,19 @@
 #pragma once
 
+#include <vector>
+#include <pugixml.hpp>
+
 #include "SelectAttributeTreeWalker.h"
 #include "SelectNameTreeWalker.h"
 #include "SelectSubstringTreeWalker.h"
 
-class DataSelecter
+namespace EbookPeriodical
 {
-public:
-	DataSelecter() noexcept;
-	~DataSelecter();
-	std::vector<pugi::xml_node> selectNodesByAttribute(pugi::xml_node source, std::string attributeName, std::string attributeValue);
-	std::vector<pugi::xml_node> selectNodesByName(pugi::xml_node source, std::string name);
-	std::vector<pugi::xml_node> selectNodesByTextSubstring(pugi::xml_node source, std::string substring);
-protected:
-	SelectAttributeTreeWalker selectAttributeTreeWalker;
-	SelectNameTreeWalker selectNameTreeWalker;
-	SelectSubstringTreeWalker selectSubstringTreeWalker;
-};
-
+	template <typename TreeWalker, typename... Args>
+	std::vector<pugi::xml_node> selectNodes(pugi::xml_node source, Args&& ... args)
+	{
+		TreeWalker walker(std::forward<Args>(args)...);
+		source.traverse(walker);
+		return walker.getResults();
+	}
+}
